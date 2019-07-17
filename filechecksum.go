31034f88cff987f6608104be4180fa8f9b12f2dc
Chih-Han Liu <andyliu@sinica.edu.tw>
2019-07-17 13:21:36 +0800

    Initial project

---
+++ filechecksum.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+   "crypto/md5"
+   "encoding/hex"
+   "fmt"
+   "io"
+   "os"
+)
+
+type MD5 struct {
+}
+
+func(md *MD5) FileMD5(filename string) (string, error) {
+   file, err := os.Open(filename)
+   if err != nil {
+      fmt.Println("os Open error")
+      return "", err
+   }
+   md5 := md5.New()
+   _, err = io.Copy(md5, file)
+   if err != nil {
+      fmt.Println("io copy error")
+      return "", err
+   }
+   md5Str := hex.EncodeToString(md5.Sum(nil))
+   return md5Str, nil
+}
+
+
+func NewMD5CheckSum()(*MD5) {
+   return &MD5 {
+   }
+}
+
+func main() {
+   file := "test.txt"
+
+   m := NewMD5CheckSum()
+ 
+   Val1, _ := m.FileMD5(file)
+ 
+   fmt.Printf("%s\n", Val1)
+}
