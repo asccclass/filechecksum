fdebccba737905bf30c994fbb06754aaa9108eef
Chih-Han Liu <andyliu@sinica.edu.tw>
2019-07-18 09:19:37 +0800

    <文件修改>: 精簡error的輸出

--- filechecksum.go
+++ filechecksum.go
@@ -14,13 +14,10 @@ type MD5 struct {
 func(md *MD5) FileMD5(filename string) (string, error) {
    file, err := os.Open(filename)
    if err != nil {
-      fmt.Println("os Open error")
       return "", err
    }
    md5 := md5.New()
-   _, err = io.Copy(md5, file)
-   if err != nil {
-      fmt.Println("io copy error")
+   if _, err = io.Copy(md5, file); err != nil {
       return "", err
    }
    md5Str := hex.EncodeToString(md5.Sum(nil))
